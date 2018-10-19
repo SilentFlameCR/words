cf87f01f528ae2c8eeb7feffd77ab7c12acb516b
SilentFlameCR <rodr0180@algonquinlive.com>
2018-10-19 09:03:37 -0400

    Updates

---
+++ precache-manifest.e71e4ea92469c240df9ceaa144e31a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdcdc1249527ceff0ca7",
+    "url": "/words/static/js/runtime~main.fdcdc124.js"
+  },
+  {
+    "revision": "80a5bc97cdcdcfa84847",
+    "url": "/words/static/js/main.80a5bc97.chunk.js"
+  },
+  {
+    "revision": "2ec1665dd101d02e5f45",
+    "url": "/words/static/js/1.2ec1665d.chunk.js"
+  },
+  {
+    "revision": "80a5bc97cdcdcfa84847",
+    "url": "/words/static/css/main.aeb554e4.chunk.css"
+  },
+  {
+    "revision": "2ec1665dd101d02e5f45",
+    "url": "/words/static/css/1.c683af95.chunk.css"
+  },
+  {
+    "revision": "e5c16cd9ef7d08e19110d21f59435d21",
+    "url": "/words/index.html"
+  }
+];
